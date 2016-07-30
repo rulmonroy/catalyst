b1e3519db23f53a627ffb4828c76d40a0c4c7952
Raul Monroy Ortega <rulmonroy@users.noreply.github.com>
2016-07-30 21:33:44 +1000

    Full functionality finished.
    Completed argument control and added full functionality according to requirements.

--- user_upload.php
+++ user_upload.php
@@ -1,10 +1,15 @@
 <?php
 	// Argument handler
 	// -----------------------------------
-	$argsok = false;
-	$optshort = " u:p:h:";
-	$optlong = array ("create_table","file:","help","dry_run");
-	$options = getopt($optshort,$optlong);
+	$argsok 	= false;
+	$optshort 	= "u:p:h:";
+	$optlong 	= array ("create_table","file:","help","dry_run");
+	$options 	= getopt($optshort,$optlong);
+	
+	$parse 		= false;		// Activate file parsing and data validation.
+	$import 	= false;		// Flag to trigger table rebuild and data import.
+	$preparedb 	= false;		// Flag to trigger database table creation.
+	$suspend	= false;		// Flag to suspend script.
 	
 	var_dump($options);
 	
@@ -12,80 +17,116 @@
 	if (array_key_exists("help",$options)) {				
 		show_help();
 	}
+	// --create_table command.
 	elseif (array_key_exists("create_table",$options)) {
 		if ( !array_key_exists("u",$options) || !array_key_exists("p",$options) || !array_key_exists("h",$options) ) {
 			echo "Database information not provided. Please, use -u -p -h to specify parameters.\nType: --help for full command line options.\n\n";
-			exit;
+			$suspend = true;
 		}
-		elseif (array_key_exists("dry_run",$options)) {
-			echo "--dry_run and --create_table commands can't be used at the same time. Please, verify and try again.\nType: --help for full command line options.\n\n";
+		elseif (array_key_exists("dry_run",$options)) {		// Can't conflict with dry_run
+			echo "dry_run and create_table commands can't be used at the same time. Please, verify and try again.\nType: --help for full command line options.\n\n";
+			$suspend = true;
 		}
-		echo "User table will be created. Do you wish to continue? (type 'yes' or 'no'): ";
-		$handle = fopen ("php://stdin","r");
-		$answer = fgets($handle);
-		fclose($handle);
-		
-		if(strtolower(trim($answer)) != 'yes'){
-			echo "Process ended by user.\n\n";
-			exit;
-		} 
+		$preparedb = true;
 	}
-	if (array_key_exists("file",$options)) {
-		echo "File provided: ".$options["file"]."\n\n";
+	// --file command.
+	elseif (array_key_exists("file",$options)) {
+		
+		if ( strtolower(substr($options["file"],strlen($options["file"])-3)) != ".csv" ) {
+			echo "Invalid file. Only CSV files are accepted.\nType: --help for more details.\n\n";
+			exit;
+		}
+		else {
+			$parse = true;
+			
+			if (!array_key_exists("dry_run",$options)) {	
+				if ( !array_key_exists("u",$options) || !array_key_exists("p",$options) || !array_key_exists("h",$options) ) {
+					echo "Database information not provided. Please, use -u -p -h to specify parameters.\nType: --help for full command line options.\n\n";
+					exit;
+				}
+				else {
+					$import = true;
+				}
+			}
+		}	
 	}
 	else {
 		echo "Filename not provided. Please, use --file [filename] to specify the file to be imported.\nType: --help for full command line options.\n\n";
 	}
 	
-	if ($argsok) {
-		// Setting db connection values
-		$servername = "localhost";
-		$username 	= "root";
-		$password 	= "raul01";
-		$dbname 	= "catalyst";
+	// import --dry_run --create_table
+	if ( ($import || $preparedb) && !$suspend ) {	
+		$dbconn = dbConnect("catalyst",$options["h"],$options["u"],$options["p"]);
+		
+		if ($dbconn != NULL) {			
+			if ( chkTableExist($dbconn,"users") ) {
+				if ($preparedb) {	// --create_table
+					echo "Users table already exists in database!\nProcess terminated.\n\n";
+					$suspend = true;
+				}
+				else {				// import
+					echo "All data in the 'users' table will be replaced. Do you wish to continue? (type 'yes' or 'no'): ";
+					$handle = fopen ("php://stdin","r");
+					$answer = fgets($handle);
+					fclose($handle);
+									
+					if(strtolower(trim($answer)) != 'yes'){	
+						echo "Process ended by user.\n\n";
+						$import = false;
+					} 
+				}
+			}
+			elseif ($preparedb) {	// --create_table
+				echo "User table will be created. Do you wish to continue? (type 'yes' or 'no'): ";
+				$handle = fopen ("php://stdin","r");
+				$answer = fgets($handle);
+				fclose($handle);
+		
+				if(strtolower(trim($answer)) != 'yes'){	
+					echo "Process ended by user.\n\n";
+					$suspend = true;
+				} 
+				else {
+					$usrexist = false;
+					
+					if ( CreateTable($dbconn) ) {
+						echo "Users table was created successfully!\n\n";
+						$suspend;
+					}
+				}
+			}
+			
+			// --------- Rebuild table ----------
+			if ( $import && !$suspend ) {	// import
+				$sql = "DROP TABLE users";
 
-		// Establishing DB Connection
-		$dbconn = new mysqli($servername, $username, $password, $dbname);
-		
-		// Check connection
-		if ($dbconn->connect_error) {
-			die("Connection failed: " . $dbconn->connect_error);
-		} 
-		
-		echo "Connected to DB successfully!!";
-		
-		// Table dropping
-		$sql = "DROP TABLE users";
-
-		// Attempt at dropping table
-		if ($dbconn->query($sql) === TRUE) {
-			echo "\n\nUsers table was dropped.";
-		} 
-		else {
-			echo "\n\nError dropping users table: " . $dbconn->error;
+				if ($dbconn->query($sql) !== TRUE) {
+					$suspend = true;
+					echo "\n\nError dropping users table: " . $dbconn->error;
+				} 	
+				else {
+					if ( CreateTable($dbconn) ) {
+						echo "Users table was created successfully!\n\n";	
+					}		
+					else {
+						$suspend = true;
+					}
+				}			
+			}
 		}
-		
-		// Table creation
-		$sql = "CREATE TABLE users (
-					email VARCHAR(50) PRIMARY KEY,
-					name VARCHAR(30) NOT NULL, 
-					surname VARCHAR(30) NOT NULL
-				)";
-				
-		// Attempt to create table
-		if ($dbconn->query($sql) === TRUE) {
-			echo "\n\nusers table created successfully!!";
-		} 
 		else {
-			echo "\n\nError creating users table: " . $dbconn->error;
+			$suspend = true;
 		}
-		
-		//---------- Loading CSV file into database
-		
-		$file = fopen('users.csv', 'r');
+	}
+	
+	//---------- Parse CSV file -----------
+	if ( $parse && !$suspend ) {				// import --dry_run
+		$file = fopen($options["file"], 'r');
 		$hdr = false;
 		$headers = array();
 		$usr_count = 0;
+		$usr_valid = 0;
+		$allEmails = array();
 		
 		while ($file != NULL && ($line = fgetcsv($file)) !== FALSE) {
 			if ($hdr == false) {
@@ -95,28 +136,54 @@
 			else {	// Insert row into table
 				$email = trim(strtolower($line[array_search('email', $headers)]));
 				
-				if (filter_var($email,FILTER_VALIDATE_EMAIL) !== false) {
+				if ( filter_var($email, FILTER_VALIDATE_EMAIL) !== false ) {										// Valid email
 					$name = trim(ucfirst(strtolower(addslashes($line[array_search('name', $headers)]))));
 					$surname = trim(ucfirst(strtolower(addslashes($line[array_search('surname', $headers)]))));
+					$usr_count++;
 					
-					$sql = "INSERT INTO users (email, name, surname) VALUES ('".$email."','".$name."','".$surname."')";
+					if ( $import ) { // import
+						$sql = "INSERT INTO users (email, name, surname) VALUES ('".$email."','".$name."','".$surname."')";
 					
-					if ($dbconn->query($sql) === TRUE) {
-						$usr_count++;
-					} 
-					else {
-						echo "\n\nError adding user: " . $dbconn->error;
-					}	
+						if ($dbconn->query($sql) === TRUE) {
+							$usr_valid++;
+						} 
+						else {
+							echo "\n\nError adding user: " . $dbconn->error;
+						}	
+					}
+					else {			// --dry_run
+						$found = array_search($email, $allEmails);
+						
+						if ( $found == false && $found != NULL ) {
+							array_push($allEmails, $email);
+							$usr_valid++;
+						}
+						else {
+							echo "\n\nError validating user: Email is duplicated. Name: " . $name . " " . $surname;
+						}					
+					}
 				}	
 				else {
-					echo "\nUser NOT ADDED!: ".$name." ".$surname." has an invalid email: ".$email;
+					if ( $import ) {	// import
+						echo "\nError adding user: ".$name." ".$surname." has an invalid email: ".$email;	
+					}
+					else {				// --dry_run
+						echo "\nError validating user: ".$name." ".$surname." has an invalid email: ".$email;
+					}
+					
 				}			
 			}
 		}
 		fclose($file);
-		$dbconn->close();
-		echo "\n\nImport completed. ".$usr_count." users added to database.\n\n";
+		
+		if ( $import ) {
+			echo "\n\nProcess completed successfully! ".$usr_valid."/".$usr_count." imported to database.\n\n";	
+		}			
 	}
+	if ($dbconn != NULL) {
+		$dbconn->close();	
+	}
+
 	
 	function show_help() {
 		echo "\nUSER UPLOAD SCRIPT HELP\n
@@ -146,5 +213,47 @@
 			Only creates users table without importing data:\n
 				user_upload.php -create_table -u root -p root -h localhost\n
 		==============================================================================================================\n\n";
+	}
+	
+	function chkTableExist($handle, $table) {
+		$res = $handle->query("SHOW TABLES LIKE '".$table."'");
+
+		if( isset($res->num_rows) ) {
+			if ( $res->num_rows > 0 )
+				return true;
+		}
+		return false;
+	}
+	
+	function dbConnect($db,$host,$user,$pass){
+		// Establishing DB Connection
+		$dbconn = new mysqli($host, $user, $pass, $db);
+		
+		// Check connection
+		if ($dbconn->connect_error) {
+			die("Connection failed: " . $dbconn->connect_error);
+			return NULL;
+		} 
+		else {
+			echo "\nConnected successfully\n";
+		}
+		return $dbconn;
+	}
+	
+	function CreateTable($dbconn) {
+		$sql = "CREATE TABLE users (
+					email VARCHAR(50) PRIMARY KEY,
+					name VARCHAR(30) NOT NULL, 
+					surname VARCHAR(30) NOT NULL
+				)";
+				
+		// Attempt to create table
+		if ($dbconn->query($sql) === TRUE) {
+			return true;
+		} 
+		else {
+			echo "\n\nError creating users table: " . $dbconn->error."/n/n";
+			return false;
+		}
 	}
 ?>
